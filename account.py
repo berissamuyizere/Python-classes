d1d11522fd5d9e939f5bea6f1ae57894b3f5adbe
BerissaMuyizere <muyizereberissa@gmail.com>
2025-06-02 17:31:43 +0300

    account transactions

--- account.py
+++ account.py
@@ -8,3 +8,25 @@ class Transaction:
     def __str__(self):
         return f"{self.date_time} - {self.transaction_type.upper()}: {self.narration} | Amount: {self.amount}"
 
+class Account:
+    def __init__(self, name, account_number):
+        self.name = name
+        self.__balance = 0
+        self.__account_number = account_number
+        self.__transactions = []
+        self.loan = 0
+        self.loan_status = "inactive"
+        self.is_frozen = False
+        self.minimum_balance = 100  
+        self.closed = False
+        self.deposit_count = 0
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            self.__transactions.append(Transaction(amount, "Deposit", "deposit"))
+            self.deposit_count += 1
+            return f"Confirmed, you have deposited {amount}. New balance is {self.__balance}"
+        return "Deposit amount must be greater than 0"
+
+ 
